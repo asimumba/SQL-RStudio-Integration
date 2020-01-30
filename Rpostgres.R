22e525d111b8a49685faa132b6fd890b972f8a65
Aaron Simumba <simumbaa21@gmail.com>
2020-01-30 18:11:44 +0200

    initial set-up

---
+++ Rpostgres.R
@@ -0,0 +1,29 @@
+library(DBI)
+# Connect to the default postgres database
+con <- dbConnect(RPostgres::Postgres())
+
+dbListTables(con)
+dbWriteTable(con, "mtcars", mtcars)
+dbListTables(con)
+
+dbListFields(con, "mtcars")
+dbReadTable(con, "mtcars")
+
+# You can fetch all results:
+res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+dbFetch(res)
+dbClearResult(res)
+
+# Or a chunk at a time
+res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+while(!dbHasCompleted(res)){
+  chunk <- dbFetch(res, n = 5)
+  print(nrow(chunk))
+}
+# Clear the result
+dbClearResult(res)
+
+# Disconnect from the database
+dbDisconnect(con)
+
+
